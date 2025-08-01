8e5fb37482383358c4983be5550e85a8e6a323a5
Andrea Rotesi <andrearotesi@gmail.com>
2025-08-01 20:06:45 +0200

    Added icons

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,8 @@ import { Carousel } from './components/carousel/carousel';
 import { Video } from './components/video/video';
 import { Divider } from './components/divider/divider';
 import { Footer } from './components/footer/footer';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {FontAwesomeModule, IconDefinition} from '@fortawesome/angular-fontawesome';
+import {faRobot, faLeaf, faGears} from '@fortawesome/free-solid-svg-icons';
 
 interface CardItems {
   color: CardColor;
@@ -29,9 +30,9 @@ export class App {
     { image: 'images/diesel.png', color: 'lilac', title: '-14.58%', subtitle: 'Consumo diesel' },
     { image: 'images/women.png', color: 'azure', title: '+19.38%', subtitle: 'Lavoratrici assunte' },
   ];
-  protected readonly companyItems: { title: string, subtitle: string }[] = [
-    { title: 'Robotics', subtitle: 'Dal 2000, la divisione Robotics sviluppa soluzioni avanzate per semplificare la vita delle persone, riducendo l’impatto ambientale e promuovendo uno stile di vita efficiente. Unendo meccatronica, intelligenza artificiale e design sostenibile, mette l’innovazione al servizio dell’uomo e del pianeta.\n' },
-    { title: 'Green Innovation', subtitle: 'Fondata nel 2017, la divisione promuove l’indipendenza energetica e la sostenibilità in ambito residenziale, commerciale e industriale. Con soluzioni intelligenti, aiuta a ridurre i consumi, migliorare l’efficienza e accelerare la transizione energetica.' },
-    { title: 'Automation', subtitle: 'La divisione Automation offre soluzioni intelligenti per la tracciabilità e l’automazione dei processi, migliorando efficienza, sicurezza e sostenibilità in ambito industriale, sanitario e ambientale.' },
+  protected readonly companyItems: { icon: IconDefinition; title: string, subtitle: string }[] = [
+    { icon: faRobot, title: 'Robotics', subtitle: 'Dal 2000, la divisione Robotics sviluppa soluzioni avanzate per semplificare la vita delle persone, riducendo l’impatto ambientale e promuovendo uno stile di vita efficiente. Unendo meccatronica, intelligenza artificiale e design sostenibile, mette l’innovazione al servizio dell’uomo e del pianeta.\n' },
+    { icon: faLeaf, title: 'Green Innovation', subtitle: 'Fondata nel 2017, la divisione promuove l’indipendenza energetica e la sostenibilità in ambito residenziale, commerciale e industriale. Con soluzioni intelligenti, aiuta a ridurre i consumi, migliorare l’efficienza e accelerare la transizione energetica.' },
+    { icon: faGears, title: 'Automation', subtitle: 'La divisione Automation offre soluzioni intelligenti per la tracciabilità e l’automazione dei processi, migliorando efficienza, sicurezza e sostenibilità in ambito industriale, sanitario e ambientale.' },
   ];
 }
