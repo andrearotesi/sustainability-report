1b688d43171ce332b47815caf162d597c390796b
Andrea Rotesi <andrearotesi@gmail.com>
2025-08-01 19:14:35 +0200

    Fixed autoplay issue

--- src/app/components/video/video.ts
+++ src/app/components/video/video.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, input, signal, ViewChild } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, input, signal, ViewChild} from '@angular/core';
 import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons';
 import { FaIconComponent, IconDefinition } from '@fortawesome/angular-fontawesome';
 
@@ -10,13 +10,18 @@ import { FaIconComponent, IconDefinition } from '@fortawesome/angular-fontawesom
   templateUrl: './video.html',
   styleUrl: './video.scss'
 })
-export class Video {
+export class Video implements AfterViewInit {
   @ViewChild('videoRef') videoRef!: ElementRef;
 
   src = input.required<string>();
 
   protected readonly isPlaying = signal<boolean>(true);
   protected readonly toggleIcon = signal<IconDefinition>(faPause);
+
+  ngAfterViewInit(): void {
+    this.videoRef.nativeElement.muted = true;
+    this.videoRef.nativeElement.play();
+  }
 
   toggleVideo(): void {
     if (this.isPlaying()) {
